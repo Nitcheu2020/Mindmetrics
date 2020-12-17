6e53bba37d3744b029d16da54c6db63b635793bd
Njinou <nitcheupascal@gmail.com>
2020-12-17 17:01:44 -0500

    factoring the text in the App to facilitate the localization

---
+++ src/text/TextKey.js
@@ -0,0 +1,36 @@
+const TextKey = {
+    "button":{
+        "takeTest":"TAKE TEST",
+        "freeAnalysis": "GET FREE ANALYSIS",
+        "beginExam":"Begin The Exam &rarr;",
+    },
+    "menu":{
+        home:'HOME',
+        hwItW:'HOW IT WORKS',
+        contact:'CONTACT'
+    },
+    "resultPage":{
+        premiumReport:"GET PREMIUM REPORTS",
+        yourName: "Your Name",
+        youAre : "You are :",
+        helpful: "Helpful and Analytical",
+        paragraph:{
+            first:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+            second:'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+            third:'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint non proident.'
+        },
+        shareResult:'Share Your Results'
+    },
+    "footer":{
+        ask:' Ask Your Friends and Family to Take the Test Too!',
+        right:'Mindmetrics Technology. All rights reserved.'
+    },
+    "testPage":{
+        takeTest:' Take The Personality Test',
+        paragraph:' Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ',
+        invitation:'Please enter your information to take the exam',
+        agree:'Agree',
+        disagree:'Disagree'
+    } 
+}
+export default TextKey;
